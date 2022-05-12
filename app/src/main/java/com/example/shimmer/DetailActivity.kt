1e132314f3703b499ee2e3616c7bfd9c14a3aab7
Fernando Noguera <90071351+Nand0B3rt0n3@users.noreply.github.com>
2022-05-12 19:52:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/shimmer/DetailActivity.kt
@@ -0,0 +1,26 @@
+package com.example.shimmer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.view.isVisible
+import com.example.shimmer.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+        showData()
+        }, 5000)
+    }
+
+    private fun showData() {
+        binding.viewLoading.isVisible = false
+        binding.viewContainer.isVisible = true
+    }
+}
